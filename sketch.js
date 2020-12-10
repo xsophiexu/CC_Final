7c39dd4a3cbbaa6a60d8da4289db53b73acf1fbf
xsophiexu <58953096+xsophiexu@users.noreply.github.com>
2020-12-10 15:26:23 -0500

    Hands

--- sketch.js
+++ sketch.js
@@ -4,45 +4,38 @@ const context = canvas.getContext("2d");
 
 //turn video on or off
 let renderVideo = true;
-
 let videoLoaded = false;
 let model = null;
-let state = 0;
 
 let liveVideo;
 let poseNet, pose, skeleton;
+let xCord, yCord, xCord2, yCord2;
 
-let xCord;
-let yCord;
-let xCord2;
-let yCord2;
-
-let outline;
-let button1;
-let button2;
-let button3;
-let sButton;
-let currentTime;
+let button1, button2, button3;
+let currentTime, startTime;
 let interval = 0;
-let saved;
-let img, mask;
-let timer;
+let state = 0;
+let saved, timer, mode, time1;
+let offset, nScore;
+let speed = 0;
+let img, img2, mask;
 
-let click;
-let bassSynth;
-let note;
-let id;
+let bassSynth, note, id;
 let song1 = [];
 let freePlay = [];
+let dodge = [];
+let misses = 0;
 let gamestate = 0;
-let mode;
-let offset;
 let score = 0;
 let highscore = 0;
+let highscore2 = 0;
 
 function restart(){
   score = 0;
+  speed = 0;
+  misses = 0;
   song1 = [];
+  dodge = [];
   beginSong();
   mode = undefined;
 }
@@ -67,6 +60,7 @@ function startVideo() {
         // console.log("video started", status);
         if (status) {
             state = 1;
+            time1 = millis();
             videoLoaded = true
             runDetection()
         } else {
@@ -124,6 +118,7 @@ function setup() {
   textFont('Montserrat');
   textStyle('normal');
   img = loadImage('blue.jpg');
+  img2 = loadImage('Hands.png');
   mask = loadImage('mask2.png');
   noStroke();
   liveVideo = createCapture(VIDEO);
@@ -133,13 +128,12 @@ function setup() {
   poseNet.on('pose', gotPoses);
   
     
-  //sButton = createButton('Start');
   button1 = createButton('Timed');
-  button2 = createButton('Free-Play');
+  button2 = createButton('Piano');
+  button3 = createButton('Dodge');
     button1.hide();
     button2.hide();
-    //sButton.hide();
-  //button3 = createButton('Dodge');
+    button3.hide();
   bassSynth = new Tone.MembraneSynth().toMaster();
   Tone.Transport.start();
   beginSong();
@@ -172,7 +166,6 @@ function setup() {
 }
 
 function beginSong(){
-  //bassSynth.triggerAttackRelease('G4', '8n');
   song1.push (new Character(1, "G4"));
   song1.push (new Character(2, "E4"));
   song1.push (new Character(3, "C3"));
@@ -188,8 +181,8 @@ if(videoLoaded == false){
   textSize(40);
   if (state === 0){
     text('Loading...', 400, 380);
-    textSize(25);
-    text('If lost check the bottom for instructions', 400, 420);
+    //textSize(25);
+    //text('If lost check the bottom for instructions', 400, 420);
   } else if (state === 1) {
     text('Loaded', 400, 380);
   } else if (state === 2) {
@@ -219,47 +212,57 @@ if(videoLoaded == false){
   if (gamestate === 0){
     button1.class('group-select');
     button2.class('group-select');
-    //button3.class('group-select');
+    button3.class('group-select');
     button1.position(0, 350);
     button2.position(0, 410);
+    button3.position(0, 470);
     
     button1.show();
     button2.show();
-    //sButton.show();
+    button3.show();
     fill(1, 75, 114);
-    strokeWeight(2.5);
     rect(600, 100, 120, 50, 4);
     fill(255);
     textSize(26);
     text('Start', 636, 133);
-
-      textAlign(RIGHT);
+    
+    textAlign(CENTER);
+    if (millis() - time1 < 10000 && (millis() % 500) < 260) {
+      image(img2,147, 147, 545, 453);
+      fill(37, 198, 254);
+      textSize(30);
+      text('Use hands as cursors',  410, 470);
+    }
+    
+    textAlign(RIGHT);
       textSize(18);
       fill(255);
       if (xCord != null){
         text('Move your hands to select a mode and press start', 780, 680);
       }
-      textAlign(LEFT);
-    if (xCord > 0 && xCord < 260 && yCord > 350 && yCord < 410){
-      mode = 'Timed';
-    } else if (xCord2 > 0 && xCord2 < 260 && yCord2 > 350 && yCord2 < 410){
-      mode = 'Timed';
+    textAlign(LEFT);
+    if (xCord > 0 && xCord < 260 && yCord > 350 && yCord < 410){ mode = 'Timed';
+    } else if (xCord2 > 0 && xCord2 < 260 && yCord2 > 350 && yCord2 < 410){ mode = 'Timed';
     }
-    if (xCord > 0 && xCord < 260 && yCord > 410 && yCord < 470){
-      mode = 'Free-Play';
-    } else if (xCord2 > 0 && xCord2 < 260 && yCord2 > 410 && yCord2 < 470){
-      mode = 'Free-Play';
+    if (xCord > 0 && xCord < 260 && yCord > 410 && yCord < 470){ mode = 'Free-Play';
+    } else if (xCord2 > 0 && xCord2 < 260 && yCord2 > 410 && yCord2 < 470) { mode = 'Free-Play';
+    }
+    if (xCord > 0 && xCord < 260 && yCord > 470 && yCord < 530){ mode = 'Dodge';
+    } else if (xCord2 > 0 && xCord2 < 260 && yCord2 > 470 && yCord2 < 530) { mode = 'Dodge';
     }
     
     if (mode == 'Timed'){
       button1.class('group-selected');
       button2.class('group-select');
-      //button3.class('group-select');
-    }
-    if (mode == 'Free-Play'){
+      button3.class('group-select');
+    } if (mode == 'Free-Play'){
       button1.class('group-select');
       button2.class('group-selected');
-      //button3.class('group-select');
+      button3.class('group-select');
+    } if (mode == 'Dodge'){
+      button1.class('group-select');
+      button2.class('group-select');
+      button3.class('group-selected');
     }
     if ((xCord > 600 && xCord < 720 && yCord > 100 && yCord < 150) || (xCord2 > 600 && xCord2 < 720 && yCord2 > 100 && yCord2 < 150)){
       if (mode == 'Timed'){
@@ -267,18 +270,26 @@ if(videoLoaded == false){
         gamestate = 1;
         button1.hide();
         button2.hide();
-        //sButton.hide();
+        button3.hide();
       } else if (mode == 'Free-Play') {
         currentTime = millis();
         gamestate = 2;
         button1.hide();
         button2.hide();
-        //sButton.hide();
-      }
+        button3.hide();
+      } else if (mode == 'Dodge') {
+        currentTime = millis();
+        startTime = millis();
+        gamestate = 3;
+        button1.hide();
+        button2.hide();
+        button3.hide();
+      } 
   }
   }
   
   if (gamestate === 1){
+    nScore = 1;
     let timePassed = millis() - currentTime;
     for (let char of song1){
       if (timePassed > 1000){
@@ -318,9 +329,9 @@ if(videoLoaded == false){
   
   if (gamestate === 1.5){
     fill(1, 75, 114);
-    strokeWeight(2.5);
     rect(600, 100, 120, 50, 4);
     rect(450, 100, 120, 50, 4);
+    noStroke();
     fill(255);
     textSize(24);
     text('Back', 635, 133);
@@ -333,7 +344,11 @@ if(videoLoaded == false){
     } else if ((xCord > 450 && xCord < 570 && yCord > 100 && yCord < 150) || (xCord2 > 450 && xCord2 < 570 && yCord2 > 100 && yCord2 < 150)){
       restart();
       currentTime = millis();
-      gamestate = 1;
+      if (nScore === 1){
+        gamestate = 1;
+      } if (nScore === 2){
+        gamestate = 3;
+      }
     }
     
     fill(230);
@@ -344,7 +359,11 @@ if(videoLoaded == false){
     textAlign(CENTER);
     textSize(30);
     fill(0);
+    if (nScore ===1){
     text('Highscore: ' + highscore, 180, 498);
+    } if (nScore ===2){
+    text('Highscore: ' + highscore2, 180, 498);
+    }
     textAlign(LEFT);
 
   }
@@ -353,37 +372,88 @@ if(videoLoaded == false){
     for(let i = 0; i < 14; i++){
       fill(255);
       stroke(2);
-      rect(70+i*45, 300, 30, 90);
+      rect(70+i*45, 380, 43, 120);
     }
+    noStroke();
     for(let i = 0; i < 13; i++){
       if (i % 7 === 6){
       } else if (i % 7 === 2){
       } else {
         fill(0);
-        rect(85+i*45, 200, 30, 90);
+        rect(95+i*45, 380, 35, 70);
       }
     }
     for (let char of freePlay){
       char.clicked();
     }
-    
+    if ((millis() - currentTime) > 4000){
+      if ((xCord > 600 && xCord < 720 && yCord > 100 && yCord < 150) || (xCord2 > 600 && xCord2 < 720 && yCord2 > 100 && yCord2 < 150)){
+      restart();
+      gamestate = 0;
+      }
+    }
     fill(1, 75, 114);
-    strokeWeight(2.5);
     rect(600, 100, 120, 50, 4);
     fill(255);
     textSize(24);
     text('Back', 635, 133);
+  }
+  
+  if (gamestate === 3) {
+    nScore = 2;
+    if (millis() - currentTime > (4000 - speed)) {
+      dodge.push (new gamestate3());
+      currentTime = millis();
+    }
     
-    if ((xCord > 600 && xCord < 720 && yCord > 100 && yCord < 150) || (xCord2 > 600 && xCord2 < 720 && yCord2 > 100 && yCord2 < 150)){
+    if ((millis() - startTime) > 6000){
+      if ((xCord > 600 && xCord < 720 && yCord > 100 && yCord < 150) || (xCord2 > 600 && xCord2 < 720 && yCord2 > 100 && yCord2 < 150)){
       restart();
       gamestate = 0;
+      }
+      fill(1, 75, 114);
+      rect(600, 100, 120, 50, 4);
+      fill(255);
+      textSize(24);
+      text('Back', 635, 133);
+    }
+    if (misses > 3){
+      gamestate = 1.5;
+    } else if (speed < 3000) {
+      speed += 10;
+    }
+    for (let char of dodge){
+      if (misses < 4){
+        char.view();
+        char.move();
+        char.score();
+      }
+    }
+    
+    fill(255);
+    textSize(25);
+    text('Score: ' + score, 40, 640);
+
+    fill(255, 0, 0);
+    textSize(45);
+    for (let i = 0; i < misses; i++){
+      if (i < 3){
+        text('X', 40 + 40*i, 690);
+      }
+    }
+    highscore2 = localStorage.getItem('Highest2');
+    if (score > highscore2) {
+      highscore2 = score;
+      localStorage.setItem('Highest2', highscore2);
     }
   }
+  
   
   if (gamestate === 1) {
     fill(58, 58, 170);
     offset = 0;
   } else if (gamestate === 2){
+    fill(58, 58, 170);
     offset = 20;
   } else {
     fill(58, 58, 170);
@@ -391,10 +461,10 @@ if(videoLoaded == false){
   }
     
   if(xCord != null && yCord != null){
-    ellipse(xCord, yCord, width/15-offset);
+    ellipse(xCord, yCord, 40-offset);
   }
   if(xCord2 != null && yCord2 != null){
-    ellipse(xCord2, yCord2, width/15-offset);
+    ellipse(xCord2, yCord2, 40-offset);
   }
   if (xCord != null){
   } else {
@@ -414,27 +484,25 @@ class Character {
   constructor (id, note) {
     this.s = 25;
     this.x = random(50, 750);
-    this.y = random(50, 630);
+    this.y = random(50, 530);
     this.c = random(80, 225);
     this.id = id;
     this.alive = true;
     this.sound = note;
-    if (this.alive === true){
-      this.start = millis();
-    } else {
-      let now = this.start;
-      this.start = 0;
-      if ((millis() - now) < 1000) {
-        fill(255);
-        rect(this.x, this.y, this.s);
-      }
-    }
   }
   
   view () {
     fill(58, 58, this.c);
     if (this.alive === true){
       rect(this.x, this.y, this.s);
+      this.start = millis();
+    } else {
+      let now = this.start;
+      this.start = 0;
+      if ((millis() - now) < 3000) {
+        fill(255);
+        rect(this.x-10, this.y-10, this.s+20);
+      }
     }
   }
   
@@ -469,25 +537,72 @@ class gamestate2 {
   constructor (note, color, column) {
     //this.id = column;
     this.sound = note;
+    this.c = color;
     if (color == "white"){
       this.x = 70+column*45;
-      this.y = 300;
+      this.y = 380;
     }
     if (color == "black"){
-      this.x = 85+column*45;
-      this.y = 200;
+      this.x = 95+column*45;
+      this.y = 380;
     }
   }
   
   clicked () {
     
-    if (xCord > this.x && xCord < this.x+30 && yCord > this.y && yCord < this.y+90){
-      if ((millis() - interval) > 800){
-      bassSynth.triggerAttackRelease(this.sound, '4n');
-      interval = millis();
-      }} else if (xCord2 > this.x && xCord2 < this.x+30 && yCord2 > this.y && yCord2 < this.y+90){
-        if ((millis() - interval) > 800){
-          bassSynth.triggerAttackRelease(this.sound, '4n');interval = millis();
+    if ((xCord > this.x && xCord < this.x+43 && yCord > this.y && yCord < this.y+120) || (xCord2 > this.x && xCord2 < this.x+43 && yCord2 > this.y && yCord2 < this.y+120)){
+      if ((millis() - interval) > 800) {
+        if (this.c == "black"){
+          if ((xCord > this.x && xCord < this.x+35 && yCord > this.y && yCord < this.y+70) || (xCord2 > this.x && xCord2 < this.x+35 && yCord2 > this.y && yCord2 < this.y+70)){
+        bassSynth.triggerAttackRelease(this.sound, '4n');
+        interval = millis();
+        }}
+        else if (this.c == "white"){
+          bassSynth.triggerAttackRelease(this.sound, '4n');
+          interval = millis();
+        }
       }}
+  }
+}
+
+class gamestate3 {
+  constructor () {
+    this.x = random(25, 775);
+    this.w = random(30, 100);
+    this.h = random(30, 100);
+    this.alive = true;
+    this.y = 0 - this.h;
+  }
+  
+  view () {
+    if (this.alive === true){
+      fill(34, 149, 199);
+      rect(this.x, this.y, this.h, this.w);
+    }
+  }
+  move () {
+    if (this.alive){
+      this.y += 20;
+    }
+    if (this.y + this.h > 570){
+      this.alive = false;
+    }
+  }
+  score(){
+  if ((xCord > this.x && xCord < this.x+this.w && yCord > this.y && yCord < this.y+this.h) || (xCord2 > this.x && xCord2 < this.x+this.w && yCord2 > this.y && yCord2 < this.y+this.h)){
+    if ((millis() - interval) > 800) {
+      misses += 1;
+      bassSynth.triggerAttackRelease("B3", '4n');
+      interval = millis();
+    }
+  } else {
+    if (xCord != null && xCord2 != null){
+    score += 1;
+    textAlign(RIGHT);
+    textSize(18);
+    fill(255);
+    text('Dodge falling objects', 780, 680);
+    textAlign(LEFT);
+  }}
   }
 }
